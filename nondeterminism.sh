5b3d651dc6706bb6d3fdd9a7036357fdb2c103e1
Nes Cohen <nes@muse.dev>
2020-08-03 13:10:51 -0700

    add different bug types to prevent de-duplication

--- nondeterminism.sh
+++ nondeterminism.sh
@@ -4,12 +4,53 @@ dir="$1"
 commit="$2"
 cmd="$3"
 
+function random_type {
+	num=$(($RANDOM % 10))
+	case $num in
+	0)
+		echo "RANDOM_BUG_A"
+		;;
+	1)
+		echo "RANDOM_BUG_B"
+		;;
+	2)
+		echo "RANDOM_BUG_C"
+		;;
+	3)
+		echo "RANDOM_BUG_D"
+		;;
+	4)
+		echo "RANDOM_BUG_E"
+		;;
+	5)
+		echo "RANDOM_BUG_F"
+		;;
+	6)
+		echo "RANDOM_BUG_G"
+		;;
+	7)
+		echo "RANDOM_BUG_H"
+		;;
+	8)
+		echo "RANDOM_BUG_I"
+		;;
+	9)
+		echo "RANDOM_BUG_J"
+		;;
+	*)
+		echo "RANDOM_BUG_Z"
+		;;
+	esac
+}
+
 function random_result {
 
 	lineCount="$(wc -l source.c | cut -d ' ' -f 1)"
 	lineNumber=$(($RANDOM % $lineCount))
+	bugType="$(random_type)"
 
-	jq -M -n --arg ln $lineNumber '{type: "RANDOM_BUG", message: ("Random bug was found on line " + $ln + ". It looks extremely scary."), file: "source.c", line: $ln | tonumber}'
+	jq -M -n --arg line $lineNumber --arg bug_type $bugType \
+		'{type: $bug_type, message: ("Random bug was found on line " + $line + ". It looks extremely scary."), file: "source.c", line: $line | tonumber}'
 
 }
 
